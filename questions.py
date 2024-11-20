da67da16615b0bf710edb1180afe48c0f6bf5c84
Lucas Layman <laymanl@uncw.edu>
2024-11-20 12:41:58 -0500

    added uuid to question model

--- questions.py
+++ questions.py
@@ -1,5 +1,7 @@
 """This module defines study questions for the app"""
 
+import uuid
+
 class MultipleChoiceQuestion:
     """A class to represent a multiple choice question"""
     def __init__(self, question_text: str,  answer_index: int, choices: list[str]):
@@ -10,12 +12,13 @@ class MultipleChoiceQuestion:
             answer_index (int): The index of the correct answer in the choices list
             choices (list[str]): the list of choices for the question
         """
+        self.question_id = uuid.uuid4()  # Generate a unique ID for the question
         self.question_text = question_text
         self.answer_index = answer_index
         self.choices = choices
 
     def __repr__(self) -> str:
-        return f"MultipleChoiceQuestion({self.question_text}, {self.answer_index}, {self.choices})"
+        return f"MultipleChoiceQuestion({self.question_id}, {self.question_text}, {self.answer_index}, {self.choices})"
 
 
 QUESTIONS = [
